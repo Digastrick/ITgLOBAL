0274edbdc53f084e2b678b5d7be0df1bd5892bfa
Digastrick <110335028+Digastrick@users.noreply.github.com>
2023-06-29 02:54:04 +0300

    Add files via upload
    add select and rework select

--- src/js/select.js
+++ src/js/select.js
@@ -1,12 +1,51 @@
-const select1 = document.querySelector('.select-country');
-const eze = document.querySelectorAll('option.select-country__item');
+let select = function(){
+    let selectHeader = document.querySelectorAll('.select__header');
+    let selectItem = document.querySelectorAll('.select__item');
 
-document.body.addEventListener("click", function(e) {
-    if(e.target.classList.contains("select-country")) {
-        let index = select1.selectedIndex;
-        let options = select1.options;
-        
-         options[index].textContent = options[index].value;
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle)
+    });
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)
+    });
+
+    function selectToggle(){
+        document.querySelector('.select__body').classList.toggle('is-active');
+    }
+
+    function selectChoose(){
+        let text = this.innerText,
+        select = this.closest('.select'),
+        currentText = select.querySelector('.select__current');
+        if(text == 'United States'){
+        return currentText.innerText =  'Us',
+        currentText.classList.add('IconUSA'),
+        currentText.classList.remove("IconRU" , "IconNL" , "IconBY" , "IconKZ" , "IconTR");
+        }
+        if(text == 'Россия'){
+       return currentText.innerText = 'RU' ,
+        currentText.classList.add('IconRU'),
+        currentText.classList.remove("IconUSA" , "IconNL" , "IconBY" , "IconKZ" , "IconTR");
+        }
+        if(text == 'Netherlands')
+        return currentText.innerText = 'Nt' ,
+        currentText.classList.add('IconNL'),
+        currentText.classList.remove("IconUSA" , "IconRU" , "IconBY" , "IconKZ" , "IconTR");
+        if(text == 'Беларусь')
+       return  currentText.innerText = 'By' ,
+        currentText.classList.add('IconBY'),
+        currentText.classList.remove("IconUSA" , "IconNL" , "IconRU" , "IconKZ" , "IconTR");
+        if(text == 'Казахстан')
+        return currentText.innerText = 'Kz' ,
+        currentText.classList.add('IconKZ'),
+        currentText.classList.remove("IconUSA" , "IconNL" , "IconBY" , "IconRU" , "IconTR");
+        if(text == 'Türkiye')
+        return currentText.innerText = 'Tr' ,
+        currentText.classList.add('IconTR'),
+        currentText.classList.remove("IconUSA" , "IconNL" , "IconBY" , "IconKZ" , "IconRU");
+
+        select.classList.remove('is-active');
+    }
 }
-})
 
+select();
